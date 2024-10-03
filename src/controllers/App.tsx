3e29c7d93b2bc794604c42b9fcc692d52aca5c60
Jacques Laptop <jacqueschen92@gmail.com>
2024-10-03 14:38:45 +0200

    added some style

--- src/controllers/App.tsx
+++ src/controllers/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror, { color } from '@uiw/react-codemirror';
 import { androidstudio } from '@uiw/codemirror-theme-androidstudio';
 import { javascript } from '@codemirror/lang-javascript';
 import { RouterProvider } from 'react-router-dom';
@@ -7,27 +7,52 @@ import router from './router';
 
 const extensions = [javascript ({ jsx:true })];
 
+// CSS part
+const myStyle = {
+  color:"white",
+  backgroundColor: "grey",
+  padding: "10px",
+  margin: "6%",
+  fontSize: "16px",
+  fontFamily: "Arial",
+  border: "1px solid black",
+  borderRadius: "9px"
+};
+
+const myButtons = {
+  margin : "10px",
+  padding : "12px",
+  color:"white",
+  backgroundColor:"darkblue",
+  borderRadius: "11px"
+}
+
+// MAIN part
 const  App = () => {
   const [value, setValue] = React.useState("// Write your code here...");
   const [result, setResult] = React.useState('');
+  const [valid, setValid] = React.useState("");
   const onChange = React.useCallback((val: React.SetStateAction<string>, _viewUpdate: any) => {
     console.log('val:', val);
     setValue(val);
   }, []);
   const runCode = () => {
     try {
-      console.log('Running code...');
       eval(value);
-      setResult('Code executed successfully!');
-    } catch (err) {
-      alert(err);
+      setResult('Code executed successfully! ');
+      setValid("green");
+    } catch(err) {
+      setValid("red");
+      setResult(""+err);
     }
   };
   return  <>
             <RouterProvider router={router} />
-            <CodeMirror value={value} height="200px" theme={androidstudio} extensions={extensions} onChange={onChange} />
-            <button onClick={runCode}>Run Code</button>
-            <h3>{result}</h3>
+            <div style={myStyle}>
+              <CodeMirror style={{margin: "10px"}} value={value} height="500px" theme={androidstudio} extensions={extensions} onChange={onChange} />
+              <button style={myButtons} onClick={runCode}>Run Code</button>
+              <h3 style={{color: valid, fontSize:"25px"}}>{result}</h3>
+            </div>
           </>;
 };
 
